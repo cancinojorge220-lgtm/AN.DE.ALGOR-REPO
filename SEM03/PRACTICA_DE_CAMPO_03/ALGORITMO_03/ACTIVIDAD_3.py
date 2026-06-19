eb3ee68a128ae85dd563818ea74d1ce116787971
Jota <alvaro.02.08.96@gmail.com>
2026-06-19 09:06:20 -0500

    ejercicio 4

--- SEM03/PRACTICA_DE_CAMPO_03/ALGORITMO_03/ACTIVIDAD_3.py
+++ SEM03/PRACTICA_DE_CAMPO_03/ALGORITMO_03/ACTIVIDAD_3.py
@@ -1,44 +1,31 @@
 # Lista de números
 numeros = [3, 5, 1, 2, 8, 9, 4]
-
 # Caso base: un solo elemento
 def max_min(inicio, fin):
-
     # Si solo hay un elemento
     if inicio == fin:
         return numeros[inicio], numeros[inicio]
-
     # Si hay dos elementos
     if fin == inicio + 1:
         if numeros[inicio] > numeros[fin]:
             return numeros[inicio], numeros[fin]
         else:
             return numeros[fin], numeros[inicio]
-
     # Dividir la lista en dos partes
     medio = (inicio + fin) // 2
-
     # Resolver cada mitad
     max_izq, min_izq = max_min(inicio, medio)
     max_der, min_der = max_min(medio + 1, fin)
-
     # Combinar resultados
     if max_izq > max_der:
         maximo = max_izq
     else:
         maximo = max_der
-
     if min_izq < min_der:
         minimo = min_izq
     else:
         minimo = min_der
-
     return maximo, minimo
-
-
 # Llamada inicial
 maximo, minimo = max_min(0, len(numeros) - 1)
-
-print("Lista:", numeros)
-print("Máximo:", maximo)
-print("Mínimo:", minimo)
+print("Lista:", numeros," Máximo:", maximo," Mínimo:", minimo)
